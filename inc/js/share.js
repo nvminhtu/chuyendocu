4e1b39424ce744fa01aee53c6eb8d91ac3f7789c
Tu Nguyen <nvminhtu@gmail.com>
2017-07-31 13:20:00 +0700

    thay đổi ko load slide ở mobile mà cho load hết products

--- inc/js/share.js
+++ inc/js/share.js
@@ -24,76 +24,68 @@
   },
 
   carousel = function() {
-    $('.product_carousel').slick({
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      autoplay: false,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
+    if($(window).width() > 767) {
+       $('.product_carousel').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        autoplay: false,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
           }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
+        ]
+      });
+    } else {
 
-      ]
-    });
+    }
+   
    };
 
    topcarousel = function() {
-    $('.top_carousel').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      autoplay: true,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
+    if($(window).width() > 767) {
+      $('.top_carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        autoplay: true,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
           }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
 
-      ]
-    });
+        ]
+      });
+    }
    };
 
   slideClick = function() {
